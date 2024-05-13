24c5b78d746430b04599208b623a2f23ad30f6aa
zanciks <zackarysemancik@gmail.com>
2024-05-13 01:16:10 -0600

    Hello world asm + Makefile

--- Makefile
+++ Makefile
@@ -1,11 +1,9 @@
 ASM=nasm
+SRC_DIR=src
+BUILD_DIR=build
 
-SRC_DIR=src 
-BUILD_DIR=build 
-
-$(BUILD_DIR)/main_floppy.img: $(BUILD_DIR)/main.bin
-	cp $(BUILD_DIR)/main.bin $(BUILD_DIR)/main_floppy.img
-	truncate -s 1440k $(BUILD_DIR)/main_floppy.img
+all: $(BUILD_DIR)/main.bin 
+	qemu-system-x86_64 $(BUILD_DIR)/main.bin
 
 $(BUILD_DIR)/main.bin: $(SRC_DIR)/main.asm 
-	$(ASM) $(SRC_DIR)/main.asm -f bin -o $(BUILD_DIR)/main.bin
+	$(ASM) -f bin $(SRC_DIR)/main.asm -o $(BUILD_DIR)/main.bin
--- src/main.asm
+++ src/main.asm
@@ -1,11 +1,32 @@
-org 0x7C00
-bits 16
+[bits 16] ; 16 bit mode. required for bootloaders
+[org 0x7c00] ; where we are required to start our bootloader 
 
-main:
-  hlt
+start:
+  ; print hello_world data 
+  mov si, hello_world 
+  call print 
 
-.halt:
-  jmp .halt
+print:
+  ; we are going to modify these, so we want to be able to restore them
+  push si
+  push ax
 
-times 510-($-$$) db 0 
-dw 0AA55h
+  .loop:
+    lodsb ; load next character into al 
+    or al, al ; if char is null
+    jz .done ; then jump to done
+
+    mov ah, 0x0e ; set interrupt to print char (inside of VIDEO)
+    int 0x10 ; call interrupt of type VIDEO
+    jmp .loop
+
+  .done:
+    ; reset the values we modified
+    pop ax
+    pop si 
+    ret
+
+hello_world: db "Hello, world!", 13, 0 ; 13 is ENDL ; 0 is end string
+
+times (510 - ($-$$)) db 0x00 ; set all bytes to 0x00 until we have 510 
+dw 0xAA55 ; set our remaining bytes to 0xAA and 0x55, putting us to 512 bytes
